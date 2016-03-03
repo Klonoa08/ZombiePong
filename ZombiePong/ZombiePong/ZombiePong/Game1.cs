454ad869ec1f291fae2f7e357eef69b35d6655b2
klonoa3ds <klonoa3ds@gmail.com>
2016-03-03 11:35:19 -0500

    Zombiepong
    zombie pong - make collision detection

--- ZombiePong/ZombiePong/ZombiePong/Game1.cs
+++ ZombiePong/ZombiePong/ZombiePong/Game1.cs
@@ -61,9 +61,14 @@ namespace ZombiePong
 
             paddle1 = new Sprite(new Vector2(20, 20), spritesheet, new Rectangle(0, 516, 25, 150), Vector2.Zero);
             paddle2 = new Sprite(new Vector2(970, 20), spritesheet, new Rectangle(32, 516, 25, 150), Vector2.Zero);
-            ball = new Sprite(new Vector2(700, 350), spritesheet, new Rectangle(76, 510, 40, 40), new Vector2(30, 0));
+            ball = new Sprite(new Vector2(700, 50), spritesheet, new Rectangle(76, 510, 40, 40), new Vector2(210, 0));
+
+            
+          
+                
 
             SpawnZombie(new Vector2(400, 400), new Vector2(-20, 0));
+           
         }
 
         /// <summary>
@@ -100,6 +105,12 @@ namespace ZombiePong
 
             // TODO: Add your update logic here
             ball.Update(gameTime);
+            MouseState ms = Mouse.GetState();
+            paddle1.Location = new Vector2(paddle1.Location.X, ms.Y);
+            paddle2.Location = new Vector2(paddle2.Location.X, ball.Center.Y);
+
+            if (ball.Location.X > 950 - 16)
+                ball.Velocity = ball.Velocity * new Vector2(-1, 1);
 
             for (int i = 0; i < zombies.Count; i++)
             {
